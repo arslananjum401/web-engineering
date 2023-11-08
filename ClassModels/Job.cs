d363ee61bcbffb7374ec908c4bf219dc70876c80
arslananjum401 <arslananjum1515@gmail.com>
2023-11-08 15:15:59 +0500

    my project

---
+++ ClassModels/Job.cs
@@ -0,0 +1,10 @@
+namespace placement.ClassModels
+{
+    public class JobObj
+    {
+        public string JobTitle { get; set; } = "";
+        public string Location { get; set; } = "";
+        public string CompanyName { get; set; } = "";
+        public string PostedAgo { get; set; } = "";
+    }
+}
